d1a59bf1200e3c9c7a89e6f1cd0e4a888d5279ab
Ryan Morales <ryan.morales@west-mec.org>
2024-12-04 05:06:53 +0000

    fixed bounding rect

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,18 +4,38 @@ import { useEffect, useRef } from "react";
 import styles from "../styles/Modal.module.css";
 // export a default function called Modal
 export default function Modal({ isOpen, onClose, children }) {
-  console.log(isOpen);
   const dialogRef = useRef(null);
   useEffect(() => {
     const dialog = dialogRef.current;
+
     // make a if else block
     if (isOpen) {
-      dialog.showModal();
+      dialog?.showModal();
     } else {
-      dialog.close();
+      dialog?.close();
+    }
+
+    function handleClickOutside(e) {
+      const dialogDimensions = dialog?.getBoundingClientRect();
+      if (dialogDimensions) {
+        if (
+          e.clientX < dialogDimensions.left ||
+          e.clientX > dialogDimensions.right ||
+          e.clientY < dialogDimensions.top ||
+          e.clientY > dialogDimensions.bottom
+        ) {
+          onClose();
+        }
+      }
+    }
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
     }
     // check the condition that isOpen is true or false
     // call the showModal() or close() method on the dialog element based on the condition
+    return () => {
+      document.removeEventListener("mousdown", handleClickOutside);
+    };
   }, [isOpen, onClose]);
   return (
     <dialog className={styles.modal} ref={dialogRef}>
